1d436d707747218b5bd5318241d0c1c76e0ddeec
Ainhoaleft <izquierdo19668@iesmarenostrum.com>
2023-02-17 13:09:15 +0100

    Empezando la practica opcional,falta los fetchs

--- src/components/GitHub.vue
+++ src/components/GitHub.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
+        <input v-model="user" placeholder="Introduce nombre de usuario de Github" v-show="input" v-on:keydown="obtenerUsuario">
+        <div class="alert alert-warning" role="alert" v-show="advertencia">El usuario no existe</div>
+        <div class="card" style="width: 20px;">
+            <img src="" alt="" class="card-img-top">
+            <div class="card-body">
+                <h3 class="card-title">Titulo</h3>
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quae velit totam doloribus atque molestiae animi tempore odio natus mollitia explicabo dolorem accusamus ut voluptas,</p>
+                <a href="#" class="btn btn-primary">Pincha</a>
+                <a href="user.html_url" class="card-link">URL de Github</a>
+            </div>
+        </div>
         <!-- TODO: Crear componente GitHub -->
     </div>
 </template>
@@ -7,14 +18,21 @@
 <script>
 
 // TODO: Importar componente GitHubRepos
+import GitHubRepos from 'GitHubRepos.vue'
 
 export default {
     name: 'GitHub',
     components: {
+        GitHubRepos
         // TODO: Importar componente GitHubRepos
     },
     data: function() {
         return {
+            input: true,
+            advertencia: false,
+            card: false,
+            user: ""
+
             // TODO: crear variables de datos para el funcionamiento del componente
         }
     },
@@ -27,12 +45,13 @@ export default {
 
             // Obtener datos de autenticación de usuario para hacer peticiones
             // autenticadas a la API de GitHub
-            var userAuth = process.env.VUE_APP_USERNAME || "user";
+            var userAuth = process.env.VUE_APP_USERNAME || "ainhoaleft";
             var passAuth = process.env.VUE_APP_USERTOKEN || "pass";
 
             // TODO: realizar petición fetch par obtener los datos y mostrar la información en la página
             // Ejemplo de paso de datos de autorización con fetch: https://stackoverflow.com/questions/43842793/basic-authentication-with-fetch
-
+            let url = 'https://api.github.com/users/{{user}}';
+            fetch(url,{method:'GET'})
         },
         obtenerRepositorios: function() {
             // TODO: Función para obtener los repositorios del usuario desde la API de GítHub
@@ -40,13 +59,14 @@ export default {
 
             // Obtener datos de autenticación de usuario para hacer peticiones
             // autenticadas a la API de GitHub
-            var userAuth = process.env.VUE_APP_USERNAME || "user";
+            var userAuth = process.env.VUE_APP_USERNAME || "ainhoaleft";
             var passAuth = process.env.VUE_APP_USERTOKEN || "pass";
 
 
             // TODO: realizar petición fetch par obtener los datos y mostrar la información en la página
             // Ejemplo de paso de datos de autorización con fetch: https://stackoverflow.com/questions/43842793/basic-authentication-with-fetch
-
+            let url = 'https://api.github.com/users/{{user}}/repos';
+            fetch(url,{method:'GET'})
         }
     }
 }
