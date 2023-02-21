0682a4d05c81851ad66bcb6c357c00ad67bfa645
Ainhoaleft <izquierdo19668@iesmarenostrum.com>
2023-02-21 23:10:50 +0100

    añadiendo más codigo 21/02/2023

--- src/components/GitHub.vue
+++ src/components/GitHub.vue
@@ -3,12 +3,12 @@
         <input v-model="user" placeholder="Introduce nombre de usuario de Github" v-show="input" v-on:keydown="obtenerUsuario">
         <div class="alert alert-warning" role="alert" v-show="advertencia">El usuario no existe</div>
         <div class="card" style="width: 20px;">
-            <img src="" alt="" class="card-img-top">
+            <img src="userData.avatar_url" alt="" class="card-img-top">
             <div class="card-body">
-                <h3 class="card-title">Titulo</h3>
+                <h3 class="card-title" href="userData.login">Titulo</h3>
                 <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quae velit totam doloribus atque molestiae animi tempore odio natus mollitia explicabo dolorem accusamus ut voluptas,</p>
-                <a href="#" class="btn btn-primary">Pincha</a>
-                <a href="user.html_url" class="card-link">URL de Github</a>
+                <a href="#" class="btn btn-primary" v-on:keydown="obtenerUsuario">Pincha</a>
+                <a href="userData.html_url" class="card-link">URL de Github</a>
             </div>
         </div>
         <!-- TODO: Crear componente GitHub -->
@@ -28,10 +28,12 @@ export default {
     },
     data: function() {
         return {
+            user: 'vuejs',
             input: true,
             advertencia: false,
             card: false,
-            user: ""
+            userData: "",
+            repolist: "" 
 
             // TODO: crear variables de datos para el funcionamiento del componente
         }
@@ -45,7 +47,7 @@ export default {
 
             // Obtener datos de autenticación de usuario para hacer peticiones
             // autenticadas a la API de GitHub
-            var userAuth = process.env.VUE_APP_USERNAME || "ainhoaleft";
+            var userAuth = process.env.VUE_APP_USERNAME || "Ainhoaleft";
             var passAuth = process.env.VUE_APP_USERTOKEN || "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";
 
             // TODO: realizar petición fetch par obtener los datos y mostrar la información en la página
@@ -53,9 +55,13 @@ export default {
             let url = 'https://api.github.com/users/{{user}}';
             fetch(url,{method:'GET'})
             .then(response => response.json())
-            .then(data =>{this.user = data
+            .then(data =>{
+                let avatar_url = data.avatar_url
+                let login = data.login
+                let html_url = data.html_url
+                let repos_url = data.repos_url
                 
-            })
+            });
         },
         obtenerRepositorios: function() {
             // TODO: Función para obtener los repositorios del usuario desde la API de GítHub
@@ -63,7 +69,7 @@ export default {
 
             // Obtener datos de autenticación de usuario para hacer peticiones
             // autenticadas a la API de GitHub
-            var userAuth = process.env.VUE_APP_USERNAME || "ainhoaleft";
+            var userAuth = process.env.VUE_APP_USERNAME || "Ainhoaleft";
             var passAuth = process.env.VUE_APP_USERTOKEN || "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";
 
 
--- src/components/GitHubRepos.vue
+++ src/components/GitHubRepos.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <div class="list-group">
-            <a href="#" class="list-group-item list-group-item-action active">
-                Repositorios de este usuario
+        <div class="list-group" v-for="list in repolist" :key="list.id">
+            <a href="list.html_url" class="list-group-item list-group-item-action active" title="list.description">
+                {{list.full_name}}
             </a>
-            <a href="#" class="list-group-item list-group-item-action active">Cosas</a>
+            <a href="#" class="list-group-item list-group-item-action active">{{ list.forks_count }}</a>
             <a href="#" class="list-group-item list-group-item-action active">Mas cosas</a>
             <a href="#" class="list-group-item list-group-item-action active">Mas mas cosas</a>
         </div>
@@ -15,7 +15,7 @@
 <script>
 export default {
     name: 'GitHubRepos',
-    props: [
+    props: [ "repolist"
         // TODO: crear parámetro 'repolist'
     ],
 }
